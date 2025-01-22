052f09c1a5459ca367e9147b587e8a464885f033
Leandro <63678585+Liuth16@users.noreply.github.com>
2025-01-22 12:02:53 -0300

    Add reverseString and calculator functions and tests

--- src/functionsToTest.js
+++ src/functionsToTest.js
@@ -10,4 +10,39 @@ function capitalize(string) {
   return capitalized;
 }
 
-export { capitalize };
+function reverseString(string) {
+  if (typeof string !== "string") {
+    throw new Error("Input must be a string");
+  }
+  return string.split("").reverse().join("");
+}
+
+function calculator() {
+  function validateNumbers(...args) {
+    if (!args.every((arg) => typeof arg === "number")) {
+      throw new Error("All arguments must be numbers");
+    }
+  }
+
+  function add(a, b) {
+    validateNumbers(a, b);
+    return a + b;
+  }
+  function subtract(a, b) {
+    validateNumbers(a, b);
+    return a - b;
+  }
+
+  function multiply(a, b) {
+    validateNumbers(a, b);
+    return a * b;
+  }
+
+  function divide(a, b) {
+    validateNumbers(a, b);
+    return a / b;
+  }
+  return { add, subtract, multiply, divide };
+}
+
+export { capitalize, reverseString, calculator };
--- src/practice.test.js
+++ src/practice.test.js
@@ -1,4 +1,4 @@
-import { capitalize } from "./functionsToTest";
+import { capitalize, reverseString, calculator } from "./functionsToTest";
 
 test("capitalize is defined", () => {
   expect(capitalize).toBeDefined();
@@ -22,4 +22,51 @@ test("capitalize return the str with only the first letter capitalized", () => {
   expect(capitalize(allLower)).toBe("String with all lower case");
   expect(capitalize(allUpper)).toBe("String with all upper case");
   expect(capitalize(mixedCase)).toBe("String with mixed case");
+});
+
+test("reverseString is defined", () => {
+  expect(reverseString).toBeDefined();
+});
+
+test("reverse string accepts string only", () => {
+  const stringArgument = "this is a string argument";
+  expect(() => reverseString(stringArgument)).not.toThrow(Error);
+  expect(() => reverseString(33)).toThrow(Error);
+  expect(() => reverseString()).toThrow(Error);
+  expect(() => reverseString(null)).toThrow(Error);
+  expect(() => reverseString({})).toThrow(Error);
+  expect(() => reverseString(true)).toThrow(Error);
+});
+
+test("reverseString should return the string reversed, keeping the letter case unchanged", () => {
+  const allLower = "string with all lower case";
+  const allUpper = "STRING WITH ALL UPPER CASE";
+  const mixedCase = "stRIng wiTH mIXED CasE";
+
+  expect(reverseString(allLower)).toBe("esac rewol lla htiw gnirts");
+  expect(reverseString(allUpper)).toBe("ESAC REPPU LLA HTIW GNIRTS");
+  expect(reverseString(mixedCase)).toBe("EsaC DEXIm HTiw gnIRts");
+});
+
+test("calculator is defined", () => {
+  expect(calculator).toBeDefined();
+});
+
+test("calculator operations accepts only numbers", () => {
+  const calc = calculator();
+  expect(() => calc.add(1, 2)).not.toThrow(Error);
+  expect(() => calc.add(0.5, 0.3)).not.toThrow(Error);
+  expect(() => calc.add()).toThrow(Error);
+  expect(() => calc.add("string")).toThrow(Error);
+  expect(() => calc.add(null)).toThrow(Error);
+  expect(() => calc.add({})).toThrow(Error);
+  expect(() => calc.add(true)).toThrow(Error);
+});
+
+test("calculator can add, subtract, divide and multiply", () => {
+  const calc = calculator();
+  expect(calc.add(10, 2)).toBe(12);
+  expect(calc.subtract(15, 3)).toBe(12);
+  expect(calc.multiply(3, 5)).toBe(15);
+  expect(calc.divide(100, 4)).toBe(25);
 });
