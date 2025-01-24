4362f60e7d45fbc54a09f0e0e71bef43436b84ef
Leandro <63678585+Liuth16@users.noreply.github.com>
2025-01-23 22:07:17 -0300

    Add caesarCipher and analyzeArray function and tests

--- src/functionsToTest.js
+++ src/functionsToTest.js
@@ -45,4 +45,50 @@ function calculator() {
   return { add, subtract, multiply, divide };
 }
 
-export { capitalize, reverseString, calculator };
+function caesarCipher(string, key) {
+  if (
+    typeof string !== "string" ||
+    Number.isInteger(key) === false ||
+    key < 0
+  ) {
+    throw new Error("Incorrect argument, enter a string and a integer!");
+  }
+  let cipheredString = "";
+  const alphabet = Array.from({ length: 26 }, (_, i) =>
+    String.fromCharCode(97 + i)
+  );
+  const alphabetUpper = alphabet.map((element) => element.toUpperCase());
+  string.split("").forEach((char) => {
+    if (alphabet.includes(char)) {
+      cipheredString += alphabet[(alphabet.indexOf(char) + key) % 26];
+    } else if (alphabetUpper.includes(char)) {
+      cipheredString += alphabetUpper[(alphabetUpper.indexOf(char) + key) % 26];
+    } else {
+      cipheredString += char;
+    }
+  });
+  return cipheredString;
+}
+
+function analyzeArray(array) {
+  if (Array.isArray(array) === false) {
+    throw new Error("Please pass an array as argument");
+  }
+  if (
+    !array.every((element) => typeof element === "number") ||
+    array.length === 0
+  ) {
+    throw new Error("All array elements need to be numbers");
+  }
+  const obj = {};
+  obj.length = array.length;
+  obj.min = Math.min(...array);
+  obj.max = Math.max(...array);
+  obj.average =
+    array.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    }, 0) / array.length;
+  return obj;
+}
+
+export { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
--- src/practice.test.js
+++ src/practice.test.js
@@ -1,4 +1,10 @@
-import { capitalize, reverseString, calculator } from "./functionsToTest";
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} from "./functionsToTest";
 
 test("capitalize is defined", () => {
   expect(capitalize).toBeDefined();
@@ -69,4 +75,48 @@ test("calculator can add, subtract, divide and multiply", () => {
   expect(calc.subtract(15, 3)).toBe(12);
   expect(calc.multiply(3, 5)).toBe(15);
   expect(calc.divide(100, 4)).toBe(25);
+});
+
+test("caesarCipher is defined", () => {
+  expect(caesarCipher).toBeDefined();
+});
+
+test("caesarCipher accepts only string and a positive integer as arguments", () => {
+  expect(() => caesarCipher()).toThrow(Error);
+  expect(() => caesarCipher("string", 0.5)).toThrow(Error);
+  expect(() => caesarCipher("string", -3)).toThrow(Error);
+});
+
+test("caesarCipher take a string and number N and shifts each character by N amount", () => {
+  expect(caesarCipher("a", 3)).toBe("d");
+  expect(caesarCipher("a", 0)).toBe("a");
+  expect(caesarCipher("A", 3)).toBe("D");
+  expect(caesarCipher("z", 3)).toBe("c");
+  expect(caesarCipher("abc", 3)).toBe("def");
+  expect(caesarCipher("ABC", 3)).toBe("DEF");
+  expect(caesarCipher("a,bc!", 3)).toBe("d,ef!");
+  expect(caesarCipher("A,BC!", 3)).toBe("D,EF!");
+});
+
+test("analyzeArray is defines", () => {
+  expect(analyzeArray).toBeDefined();
+});
+
+test("analyzeArray accepts only array of numbers as argument", () => {
+  expect(() => analyzeArray([30, 40])).not.toThrow(Error);
+  expect(() => analyzeArray([])).toThrow(Error);
+  expect(() => analyzeArray()).toThrow(Error);
+  expect(() => analyzeArray({})).toThrow(Error);
+  expect(() => analyzeArray(1)).toThrow(Error);
+  expect(() => analyzeArray("string")).toThrow(Error);
+  expect(() => analyzeArray([true, false])).toThrow(Error);
+});
+
+test("analyzeArray return average, min, max e length", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
